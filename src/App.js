5d32f915f462707c719f875ac5ae62d41187ae3f
AlonaCh <aliona.chubenko@gmail.com>
2022-12-27 14:38:10 +0200

    Added repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        Coded by Alona Chubenko and is
+        <a
+          href="https://github.com/AlonaCh/react-weather-project.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-soursed on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
