48de6ac7fd9667ccef1f04f5941e708d154f79f6
AlonaCh <aliona.chubenko@gmail.com>
2023-01-02 11:56:36 +0200

    Added some props to the component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Dakar"/>
 
         <footer>
           Coded by Alona Chubenko and is
